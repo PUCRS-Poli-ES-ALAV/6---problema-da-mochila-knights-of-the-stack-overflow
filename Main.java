f0420b074fef218ea8b831a24ab8d02e16a0dc84
Anderson sprenger <anderson.sprenger@edu.pucrs.br>
2024-09-04 20:42:58 -0300

    ex2 code + tests

--- Main.java
+++ Main.java
@@ -1,27 +1,68 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Main {
 
-// FIBO-REC (n)
-//     se n ≤ 1
-//     então 
-//         devolva n
-//     senão 
-//         a ← FIBO-REC (n − 1)
-//         b ← FIBO-REC (n − 2)
-//         devolva a + b
+    // FIBO-REC (n)
+    //     se n ≤ 1
+    //     então 
+    //         devolva n
+    //     senão 
+    //         a ← FIBO-REC (n − 1)
+    //         b ← FIBO-REC (n − 2)
+    //         devolva a + b
 
-public static int fiboRec(int n) {
-    if (n <= 1) {
-        return n;
+    public static int fiboRec(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int a = fiboRec(n - 1);
+        int b = fiboRec(n - 2);
+
+        return a + b;
     }
 
-    int a = fiboRec(n - 1);
-    int b = fiboRec(n - 2);
+    // FIBO (n)
+    //     f [0] ← 0 
+    //     f [1] ← 1
+    //     para i ← 2 até n faça
+    //         f[i] ← f[i-1]+f[i-2]
+    //     devolva f [n]
 
-    return a + b;
-}
+    public static int fibo(int n) {
+        Map<Integer, Integer> f = new HashMap<>();
+        f.put(0, 0);
+        f.put(1, 1);
+
+        for (Integer i = 2; i <= n; i++) {
+            Integer a = f.get(i - 1);
+            Integer b = f.get(i - 2);
+
+            f.put(i, a + b);
+        }
+
+        return f.get(n);
+    }
+
+    // MEMOIZED-FIBO (n)
+    //     para i ← 0 até n faça
+    //         f [i] ← −1
+    //     devolva LOOKUP-FIBO (f, n)
+
+    // LOOKUP-FIBO (f, n)
+    //     se f [n] ≥ 0
+    //         então devolva f [n]
+    //     se n ≤ 1
+    //         então f [n] ← n
+    //     senão 
+    //         f [n] ← LOOKUP-FIBO(f, n − 1) + LOOKUP-FIBO(f, n − 2)
+    //     devolva f [n]
+
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello world!\n");
@@ -38,6 +79,16 @@ public static int fiboRec(int n) {
             long endTime = System.currentTimeMillis();
             long execTime = endTime - startTime;
             System.out.println("fibRec " + i.intValue() + ", "+ fiboRec(i.intValue()) + ", " + execTime + "ms;");
+        }
+
+        System.out.println("\n");
+
+        for (Integer i : tests) {
+            long startTime = System.currentTimeMillis();
+            int fib = fiboRec(i.intValue());
+            long endTime = System.currentTimeMillis();
+            long execTime = endTime - startTime;
+            System.out.println("fib " + i.intValue() + ", "+ fibo(i.intValue()) + ", " + execTime + "ms;");
         }
     }
 }
